80c594b63b50facf922112e57a424745434e32cd
Aliaksandr Lukyanau <alexfreshice@gmail.com>
2023-05-27 00:37:08 +0200

    del VacListItem

--- src/components/VacancyListItem/index.ts
+++
@@ -1,7 +0,0 @@
-import { FC } from "react";
-import { VacancyListItemType } from "./types";
-import { VacancyListItemComponent } from "./VacancyListItemComponent";
-
-export const VacancyListItem: FC<VacancyListItemType> = (props) => {
-  return VacancyListItemComponent(props);
-};
--- src/components/VacancyListItem/types.ts
+++
@@ -1,5 +0,0 @@
-import { VacancyType } from "../../types";
-
-export type VacancyListItemType = {
-  vacancy: VacancyType;
-};
